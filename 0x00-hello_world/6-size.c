9bc1fe6ae14f218e5fc77ab4ee3a8693729a1aa3
Diegokernel <777@holbertonschool.com>
2019-02-06 21:34:30 +0000

    proyectoc

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,13 +8,12 @@
  */
 int main(void)
 {
-	int a = 1, b = 4, c = 8;
 	char *d = "byte(s)";
 
-	printf("Size of a char: %i %s\n", a, d);
-	printf("Size of an int: %i %s\n", b, d);
-	printf("Size of a long int: %i %s\n", c, d);
-	printf("Size of a long long int: %i %s\n", c, d);
-	printf("Size of a float: %i %s\n", b, d);
+	printf("Size of a char: %i %s\n", sizeof(char), d);
+	printf("Size of an int: %i %s\n", sizeof(int), d);
+	printf("Size of a long int: %i %s\n", sizeof(long int), d);
+	printf("Size of a long long int: %i %s\n", sizeof(long long int), d);
+	printf("Size of a float: %i %s\n", sizeof(float), d);
 	return (0);
 }
