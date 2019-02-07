56af14e898a9aeaa14afde24db28f14d64ec10b8
Diegokernel <777@holbertonschool.com>
2019-02-07 19:18:56 +0000

    numeros

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-        int n;
+	int n;
 
-        for (n = 48 ; n < 58 ; n++)
+	for (n = 48 ; n < 58 ; n++)
 		putchar(n);
 	putchar(10);
-        return (0);
+	return (0);
 }
