0ccc15eeaa6c84020595b71c45a4b500e9d57d3d
Diegokernel <777@holbertonschool.com>
2019-02-12 20:05:04 +0000

    correction

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,12 +10,12 @@ void jack_bauer(void)
 
 	for (i = 0 ; i < 24 ; i++)
 		for (j = 0 ; j < 60 ; j++)
-				{
-					_putchar((i / 10) + '0');
-					_putchar((i % 10) + '0');
-					_putchar(58);
-					_putchar((j / 10) + '0');
-					_putchar((j / 10) + '0');
-					_putchar(10);
-				}
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(58);
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar(10);
+		}
 }
