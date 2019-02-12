cc14ea06f82851ad788ccddcb1f056915d5b0260
Diegokernel <777@holbertonschool.com>
2019-02-12 19:45:11 +0000

    sumar

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - print alphabet 10 times
+ * @i : parameter
+ * @j : parameter
+ * Return: Always 0.
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+}
