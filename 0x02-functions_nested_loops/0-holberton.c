95b36fcc64e34ec40ae7202d4d9f7a316436c62d
Diegokernel <777@holbertonschool.com>
2019-02-12 14:19:19 +0000

    correction

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,6 +8,15 @@
  */
 int main(void)
 {
-	puts ("Holberton");
+	_putchar(72);
+	_putchar(111);
+	_putchar(108);
+	_putchar(98);
+	_putchar(101);
+	_putchar(114);
+	_putchar(116);
+	_putchar(111);
+	_putchar(110);
+	_putchar(10);
 	return (0);
 }
