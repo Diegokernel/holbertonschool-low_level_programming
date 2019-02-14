85e9bd8036281751187c2363cc6451b1a479b293
Diegokernel <777@holbertonschool.com>
2019-02-14 19:30:10 +0000

    upper

--- 0x03-more_functions_nested_loops/0-isupper.c
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
+ * _isupper - check the code for Holberton School students.
+ * @c : check
  * Return: Always 0.
  */
 int _isupper(int c)
 {
-  if (c >= 'A' && c <= 'Z')
-    return (1);
-  else
-    return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
