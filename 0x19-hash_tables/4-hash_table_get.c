d896c7faa56184df9c5226a11c8ee2014ff7585c
Diegokernel <777@holbertonschool.com>
2019-05-31 20:09:34 -0500

    hash

---
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table
+ * @key: key to be added to hash table
+ * Return: value if it succeeded NULL otherwise
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index, i;
+	hash_node_t *node;
+
+	if (!ht || !key)
+		return (NULL);
+	index = key_index((const unsigned char *) key, ht->size);
+	node = ht->array[index];
+        for (nodes = 0; list; nodes++)
+	{
+		if (strcmp(list->key, key) == 0)
+		{
+			return (list->value);
+		}
+		list = list->next;
+	}
+	return (NULL);
+}
