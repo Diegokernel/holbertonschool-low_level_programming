f21cc6525f1850d1b8e8f8665e520c1c74fe2971
Diegokernel <47120841+Diegokernel@users.noreply.github.com>
2019-06-20 12:01:28 -0500

    Update 0-bubble_sort.c

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -20,7 +20,7 @@ void bubble_sort(int *array, size_t size)
 			{
 				temp = array[j];
 				array[j] = array[j - 1];
-				array[j - 1] = n;
+				array[j - 1] = temp;
 				print_array(array, size);
 			}
 		}
